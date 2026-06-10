564c23fb6b8a5403fbcbcc65dc3ea6fa7f161ec1
Luis Manuel <luismanuel@SGBR.intra>
2026-06-10 17:21:36 -0300

    Ajustes varios 2

--- tests/setup.spec.ts
+++ tests/setup.spec.ts
@@ -4,6 +4,6 @@ import { loginCompleto } from '../utils/loginCompleto';
 test('Setup login', async ({ page }) => {
   await page.setViewportSize({ width: 1920, height: 1080 });
   await loginCompleto(page);  
-  console.log('FEZ LOGIN COMPLETO OK');  
+  console.log('AUTENTICAÇÃO OK');  
   await page.waitForTimeout(4000);
 });
