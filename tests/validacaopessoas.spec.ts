04fc90569a69e4ca7e06d20f31b0f11952c611b5
Luis Manuel <luismanuel@SGBR.intra>
2026-06-11 10:27:40 -0300

    Adicionar testes perfil de acesso

--- tests/validacaopessoas.spec.ts
+++ tests/validacaopessoas.spec.ts
@@ -5,9 +5,6 @@ test('Validação cadastro de pessoas', async ({ page }) => {
   await page.setViewportSize({ width: 1920, height: 1080 });
   await loginCompleto(page);
 
-  // =============================
-  // ACESSAR CADASTRO
-  // =============================
   await Promise.all([
     page.waitForURL(/pessoa/, { timeout: 15000 }),
     page.locator('a[href*="pessoa"]').first().click()
@@ -17,18 +14,12 @@ test('Validação cadastro de pessoas', async ({ page }) => {
     page.waitForURL(/cadastro/, { timeout: 15000 }),
     page.locator('a[href*="pessoa/cadastro"]').click()
   ]);
-
-  // =============================
-  // PREENCHER PARCIAL (COM ERRO)
-  // =============================
-
-  // natureza
+  
   await page.locator('[aria-label="Natureza"]').click({ force: true });
   await page.locator('.q-menu:visible .q-item')
     .filter({ hasText: /não contribuinte/i })
     .click({ force: true });
-
-  // tipo doc
+  
   await page.locator('[aria-label="Tipo do documento de identificação"]').click({ force: true });
   await page.locator('.q-menu').last()
     .locator('.q-item')
@@ -40,25 +31,29 @@ test('Validação cadastro de pessoas', async ({ page }) => {
 
   await page.locator('[aria-label="Tipo de operação"]').click({ force: true });
   await page.waitForTimeout(700);
-    await page.locator('[aria-label="Tipo de operação"]').click({ force: true });
-    const menuDoc1 = page.locator('.q-menu').last();
-    await menuDoc1.waitFor();
-    await menuDoc1
-    .locator('.q-item')
-    .filter({ hasText: /B2C/i })
-    .click({ force: true }); 
-    console.log('TIPO DE OPERAÇÃO OK');
+  await page.locator('[aria-label="Tipo de operação"]').click({ force: true });
+  const menuDoc1 = page.locator('.q-menu').last();
+  await menuDoc1.waitFor();
+  await menuDoc1
+  .locator('.q-item')
+  .filter({ hasText: /B2C/i })
+  .click({ force: true }); 
+  console.log('TIPO DE OPERAÇÃO OK');
 
+  await page.evaluate(() => {
+  window.scrollTo({
+  top: document.body.scrollHeight,
+  behavior: 'smooth'
+  });
+  });
 
   await page.locator('.q-btn')
-    .filter({ hasText: /salvar|guardar/i })
-    .click({ force: true });
-
+  .filter({ hasText: /salvar|guardar/i })
+  .click({ force: true });
   console.log('TENTOU SALVAR COM ERRO');
-
   
   await page.waitForTimeout(7000);
-  
+
   console.log('VALIDAÇÃO OK');
 
 });
