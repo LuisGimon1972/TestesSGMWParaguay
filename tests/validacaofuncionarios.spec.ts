67c75aca3e6191fd336a77aa41f2cdb836df157f
Luis Manuel <luismanuel@SGBR.intra>
2026-06-12 08:00:53 -0300

    Adicionar testes validacao funcionarios

---
+++ tests/validacaofuncionarios.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+import { loginCompleto } from '../utils/loginCompleto';
+
+test('Validação de cadastro de funcionários', async ({ page }) => {
+  await page.setViewportSize({ width: 1920, height: 1080 });
+  await loginCompleto(page);
+
+  await page.getByText(/funcionários/i).click({ force: true });
+  const btnCadastrar = page.getByText(/cadastrar funcionário/i).first();
+  await btnCadastrar.click();
+  
+  const nomefuncionario = `TEST FUNCIONARIO  ${Date.now()}`;
+  const camponomefuncionario = page
+  .locator('.q-field')
+  .filter({ hasText: /funcionário/i })
+  .first()
+  .locator('input');
+  await expect(camponomefuncionario).toBeVisible();
+  await camponomefuncionario.fill(nomefuncionario);  
+
+  console.log('CAMPO NOME FUNCIOÁRIO PREENCHIDO OK', nomefuncionario);  
+
+  const campoCargo = page.locator('.q-field').filter({ hasText: /cargo/i }).last().locator('input');
+  await campoCargo.fill('A');
+  console.log('CAMPO CARGO COM VALOR INVALIDO OK', campoCargo);  
+
+  console.log('CAMPO CÉDULA DE IDENTIDAD VAZIO OK');  
+
+  console.log('CAMPO USUÁRIO ATRELADO VAZIO OK');    
+  
+  await page.locator('.q-btn').filter({ hasText: /salvar|guardar/i }).click({ force: true });
+
+  await page.waitForTimeout(4000);
+  
+
+
+});
