0116a73041475f0f5e9d412ab8ed8ebfa2ad8a01
Luis Manuel <luismanuel@SGBR.intra>
2026-06-15 13:52:06 -0300

    Adicionar teste busca subgrupos

--- tests/buscasubgrupos.spec.ts
+++ tests/buscasubgrupos.spec.ts
@@ -1,34 +1,34 @@
 import { test, expect } from '@playwright/test';
 import { loginCompleto } from '../utils/loginCompleto';
 
-test('Teste de busca crítico em Perfil de Espécies', async ({ page }) => {
+test('Teste de busca crítico em Subgrupos', async ({ page }) => {
     await page.setViewportSize({ width: 1920, height: 1080 });
 
-    await loginCompleto(page);
+  await loginCompleto(page);
   
     await page.waitForTimeout(1000);
     await page.getByText(/cadastros/i).click({ force: true });
     console.log('CLICOU EM CADASTROS');
 
     await page.waitForTimeout(1000);
-    page.locator('a[href*="registros/especies"]').click()
-    console.log('CLICOU EM ESPÉCIES');
+    page.locator('a[href*="registros/subgrupos"]').click()
+    console.log('CLICOU EM SUBGRUPOS');
 
-     const primeiroNome = `TEST ESPÉCIE`;
+     const primeiroNome = `TEST SUBGRUPO`;
      await page.getByLabel(/pesquisar registro/i).fill(primeiroNome);
      await page.waitForTimeout(1000);  
      await page.keyboard.press('Enter');
      await page.waitForTimeout(1500);
-     console.log('BUSCA ESPÉCIE EXISTENTE OK:', primeiroNome);
+     console.log('BUSCA SUBGRUPO EXISTENTE OK:', primeiroNome);
 
     await page.waitForTimeout(1000);
 
-    const prodInexistente = `ESPÉCIE INEXISTENTE`;
+    const prodInexistente = `SUBGRUPO INEXISTENTE`;
     await page.getByLabel(/pesquisar registro/i).fill(prodInexistente);
     await page.waitForTimeout(1000);  
     await page.keyboard.press('Enter');
     await page.waitForTimeout(1500);
-    console.log('BUSCA ESPÉCIE INEXISTENTE OK:', prodInexistente);
+    console.log('BUSCA SUBGRUPO INEXISTENTE OK:', prodInexistente);
     
     await page.waitForTimeout(4000);  
 });
--- tests/setup.spec.ts
+++ tests/setup.spec.ts
@@ -4,7 +4,7 @@ import { capturarRequisicoesApi } from '../utils/capturaApi';
 
 test('Setup login', async ({ page }) => {
   await page.setViewportSize({ width: 1920, height: 1080 });
-  await loginCompleto(page);  
+  await loginCompleto(page);    
   console.log('AUTENTICAÇÃO OK');  
 
   await capturarRequisicoesApi(page); 
