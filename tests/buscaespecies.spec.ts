85cfe9b8992778958847e761d59558cad01bb1aa
Luis Manuel <luismanuel@SGBR.intra>
2026-06-15 13:52:57 -0300

    Adicionar teste busca subgrupos 2

--- tests/buscaespecies.spec.ts
+++ tests/buscaespecies.spec.ts
@@ -3,7 +3,6 @@ import { loginCompleto } from '../utils/loginCompleto';
 
 test('Teste de busca crítico em Perfil de Espécies', async ({ page }) => {
     await page.setViewportSize({ width: 1920, height: 1080 });
-
     await loginCompleto(page);
   
     await page.waitForTimeout(1000);
