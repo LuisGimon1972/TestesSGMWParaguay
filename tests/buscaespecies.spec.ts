1ef81dec5ee21edeb6eb1a461b51e8c88c897885
Luis Manuel <luismanuel@SGBR.intra>
2026-06-15 09:37:24 -0300

    Adicionar teste busca especies

---
+++ tests/buscaespecies.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+import { loginCompleto } from '../utils/loginCompleto';
+
+test('Teste de busca crítico em Perfil de Espécies', async ({ page }) => {
+    await page.setViewportSize({ width: 1920, height: 1080 });
+
+  await loginCompleto(page);
+  
+    await page.waitForTimeout(1000);
+    await page.getByText(/cadastros/i).click({ force: true });
+    console.log('CLICOU EM CADASTROS');
+
+    await page.waitForTimeout(1000);
+    page.locator('a[href*="registros/especies"]').click()
+    console.log('CLICOU EM ESPÉCIES');
+
+     const primeiroNome = `TEST ESPÉCIE`;
+     await page.getByLabel(/pesquisar registro/i).fill(primeiroNome);
+     await page.waitForTimeout(1000);  
+     await page.keyboard.press('Enter');
+     await page.waitForTimeout(1500);
+     console.log('BUSCA EXISTENTE OK:', primeiroNome);
+
+    await page.waitForTimeout(1000);
+
+    const prodInexistente = `ESPÉCIE INEXISTENTE`;
+    await page.getByLabel(/pesquisar registro/i).fill(prodInexistente);
+    await page.waitForTimeout(1000);  
+    await page.keyboard.press('Enter');
+    await page.waitForTimeout(1500);
+    console.log('BUSCA INEXISTENTE OK:', prodInexistente);
+    
+    await page.waitForTimeout(4000);  
+});
