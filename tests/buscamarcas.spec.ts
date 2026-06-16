ae67d3e88130a5ce908f8e920e93cae0e074ab85
Luis Manuel <luismanuel@SGBR.intra>
2026-06-16 08:15:28 -0300

    Adicionar teste busca marcas

---
+++ tests/buscamarcas.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+import { loginCompleto } from '../utils/loginCompleto';
+
+test('Teste de busca crítico em  Marcas', async ({ page }) => {
+    await page.setViewportSize({ width: 1920, height: 1080 });
+
+  await loginCompleto(page);
+  
+    await page.waitForTimeout(1000);
+    await page.getByText(/cadastros/i).click({ force: true });
+    console.log('CLICOU EM CADASTROS');
+
+    await page.waitForTimeout(1000);
+    page.locator('a[href*="registros/marcas"]').click()
+    console.log('CLICOU EM MARCAS'); 
+
+     const primeiroNome = `TEST MARCA`;
+     await page.getByLabel(/pesquisar registro/i).fill(primeiroNome);
+     await page.waitForTimeout(1000);  
+     await page.keyboard.press('Enter');
+     await page.waitForTimeout(1500);
+     console.log('BUSCA MARCA EXISTENTE OK:', primeiroNome);
+
+    await page.waitForTimeout(1000);
+
+    const prodInexistente = `MARCA INEXISTENTE`;
+    await page.getByLabel(/pesquisar registro/i).fill(prodInexistente);
+    await page.waitForTimeout(1000);  
+    await page.keyboard.press('Enter');
+    await page.waitForTimeout(1500);
+    console.log('BUSCA MARCA INEXISTENTE OK:', prodInexistente);
+    
+    await page.waitForTimeout(4000);  
+});
