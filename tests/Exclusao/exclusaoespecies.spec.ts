6e852c785fa2bdc3b21a0c47ea19525475ead9a5
Luis Manuel <luismanuel@SGBR.intra>
2026-06-17 16:04:33 -0300

    Adicionar teste exclusao de especies

---
+++ tests/Exclusao/exclusaoespecies.spec.ts
@@ -0,0 +1,22 @@
+import { test } from '@playwright/test';
+import { loginCompleto } from '../../utils/loginCompleto';
+import { capturarRequisicoesApi } from '../../utils/capturaApi';
+
+test('Exclusão de datos espécies', async ({ page }) => {
+    await page.setViewportSize({ width: 1920, height: 1080 });    
+    await loginCompleto(page);    
+ 
+    await page.waitForTimeout(2000);
+    await page.getByText(/Cadastros/i).click({ force: true });
+
+    await page.waitForTimeout(1000);
+    page.locator('a[href*="registros/especies"]').click()
+    console.log('CLICOU EM ESPÉCIES'); 
+
+    await page.waitForTimeout(2000);
+    await page.waitForSelector('table img[src*="trash"]', { state: 'visible' });
+    await page.locator('table img[src*="trash"]').first().click();
+
+    await capturarRequisicoesApi(page); 
+    await page.waitForTimeout(4000);    
+});
