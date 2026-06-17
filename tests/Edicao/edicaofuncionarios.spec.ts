5fd49e86b09429fbe995acbbe3fe952cba4dd2c7
Luis Manuel <luismanuel@SGBR.intra>
2026-06-17 07:47:39 -0300

    Adicionar teste edicao de funcionarios

---
+++ tests/Edicao/edicaofuncionarios.spec.ts
@@ -0,0 +1,44 @@
+import { test, expect } from '@playwright/test';
+import { loginCompleto } from '../../utils/loginCompleto';
+import { capturarRequisicoesApi } from '../../utils/capturaApi';
+
+test('Edição de datos funcionários', async ({ page }) => {
+  await page.setViewportSize({ width: 1920, height: 1080 });
+
+  await loginCompleto(page);
+  
+  await page.getByText(/funcionários/i).click({ force: true });
+  console.log('CLICOU EM FUNCIONÁRIOS');  
+  
+  await page.locator('table img[src="/icons/edit.svg"]').first().click();
+  console.log('CLICOU NO ÍCONE DE EDITAR');    
+
+  const nomefuncionario = `TEST FUNCIONARIO ALTERADO ${Date.now()}`;
+  const camponomefuncionario = page
+  .locator('.q-field')
+  .filter({ hasText: /funcionário/i })
+  .first()
+  .locator('input');
+  await expect(camponomefuncionario).toBeVisible();
+  await camponomefuncionario.fill(nomefuncionario);
+  console.log('NOME FUNCIONÁRIO ALTERADO OK:', nomefuncionario);
+
+  const cargofuncionario = `TEST CARGO ${Date.now()}`;
+  const campocargofuncionario = page
+  .locator('.q-field')
+  .filter({ hasText: /cargo/i })
+  .last()
+  .locator('input');
+  await expect(campocargofuncionario).toBeVisible();
+  await campocargofuncionario.fill(cargofuncionario);
+  console.log('CARGO FUNCIONÁRIO ALTERADO OK:', cargofuncionario); 
+  
+  
+  await page.locator('.q-btn')
+  .filter({ hasText: /salvar|guardar/i })
+  .click({ force: true });
+  console.log('CLICOU EM SALVAR USUARIO');
+
+  await capturarRequisicoesApi(page); 
+  await page.waitForTimeout(4000);  
+});
