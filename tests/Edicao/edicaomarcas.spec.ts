6caf87289aaf6f82a759b3f47cfb17f241a9d4bb
Luis Manuel <luismanuel@SGBR.intra>
2026-06-18 15:25:26 -0300

    Ajustes de grade vazia 3

--- tests/Edicao/edicaomarcas.spec.ts
+++ tests/Edicao/edicaomarcas.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { loginCompleto } from '../../utils/loginCompleto';
 import { capturarRequisicoesApi } from '../../utils/capturaApi';
 
@@ -6,24 +6,36 @@ test('Edição de datos marcas', async ({ page }) => {
     await page.setViewportSize({ width: 1920, height: 1080 });
     await loginCompleto(page);    
  
-    await page.waitForTimeout(2000);
-    await page.getByText(/Cadastros/i).click({ force: true });
+    const cadBtn = page.getByText(/cadastros/i).first();
+    await expect(cadBtn).toBeVisible();
+    await cadBtn.click();
+    console.log('CLICOU EM CADASTRO');
 
     await page.waitForTimeout(1000);
     page.locator('a[href*="registros/marcas"]').click()
     console.log('CLICOU EM MARCAS'); 
 
-    await page.locator('table img[src="/icons/edit.svg"]').first().click();
-    console.log('CLICOU NO ÍCONE DE EDITAR');    
+    await page.waitForSelector('table');    
+    await page.locator('.q-skeleton').first().waitFor({ state: 'detached', timeout: 10000 });    
+    const trashIcons = await page.locator('table img[src*="trash"]').count();
+    console.log('Quantidade de ícones de lixo:', trashIcons);
 
-    const marca = `TEST MARCA ALTERADA ${Date.now()}`;
-    await page.getByLabel(/editar marca/i).fill(marca);
-    console.log('NOME DE MARCA ALTERADA OK:', marca);           
+    if (trashIcons > 0) {     
+        await page.locator('table img[src="/icons/edit.svg"]').first().click();
+        console.log('CLICOU NO ÍCONE DE EDITAR');    
 
-    await page.locator('.q-btn')
-    .filter({ hasText: /confirmar|guardar/i })
-    .click({ force: true });
-    console.log('CLICOU EM SALVAR MARCA');  
+        const marca = `TEST MARCA ALTERADA ${Date.now()}`;
+        await page.getByLabel(/editar marca/i).fill(marca);
+        console.log('NOME DE MARCA ALTERADA OK:', marca);           
+
+        await page.locator('.q-btn')
+        .filter({ hasText: /confirmar|guardar/i })
+        .click({ force: true });
+        console.log('CLICOU EM SALVAR MARCA');  
+    }
+    else{
+        console.log('NENHUM REGISTRO ENCONTRADO NA GRADE, NADA PARA EDITAR.');  
+    }  
 
     await capturarRequisicoesApi(page); 
     await page.waitForTimeout(4000);      
