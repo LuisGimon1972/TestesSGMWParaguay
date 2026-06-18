92d1d68fa4d3a9dc8b58bdacbde87e0c8198412a
Luis Manuel <luismanuel@SGBR.intra>
2026-06-18 09:39:35 -0300

    Ajustes de grade ao excluir

--- tests/Exclusao/exclusaofuncionarios.spec.ts
+++ tests/Exclusao/exclusaofuncionarios.spec.ts
@@ -10,20 +10,24 @@ test('Exclusão de datos funcionários', async ({ page }) => {
   await page.getByText(/funcionários/i).click({ force: true });
   console.log('CLICOU EM FUNCIONÁRIOS');  
   
-  await page.waitForTimeout(1000);        
+  await page.waitForTimeout(2000);
+  
+  const menuTresPontos = page.locator('table tr:first-child >> text=more_vert');
 
-  await page.waitForSelector('table', { state: 'visible' });
-  const menuTresPontos = page.locator('table tr:first-child >> text=more_vert').first();
-  console.log('LOCALIZOU OS TRÊS PONTOS');
-  await menuTresPontos.click();
-  console.log('CLICOU NOS TRÊS PONTOS');
+  if (await menuTresPontos.count() > 0 && await menuTresPontos.isVisible()) {
+      console.log('LOCALIZOU OS TRÊS PONTOS');
+      await menuTresPontos.click();
+      console.log('CLICOU NOS TRÊS PONTOS');
 
-  await page.waitForTimeout(1000);    
-  await page.waitForSelector('text=Excluir', { state: 'visible' });
-  await page.locator('text=Excluir').click();
-  console.log('CLICOU EM EXCLUIR');   
-  await page.waitForTimeout(2000);     
+      await page.waitForTimeout(1000);
+      await page.waitForSelector('text=Excluir', { state: 'visible' });
+      await page.locator('text=Excluir').click();
+      console.log('CLICOU EM EXCLUIR');
 
-  await capturarRequisicoesApi(page);   
-  await page.waitForTimeout(4000);  
+      await page.waitForTimeout(2000);
+      await capturarRequisicoesApi(page);
+      await page.waitForTimeout(4000);
+  } else {
+      console.log('NENHUM REGISTRO ENCONTRADO NA GRADE, NADA PARA EXCLUIR.');
+  }
 });
--- tests/Exclusao/exclusaoprodutos.spec.ts
+++ tests/Exclusao/exclusaoprodutos.spec.ts
@@ -13,19 +13,24 @@ test('Exclusão de datos Produtos', async ({ page }) => {
       ]);
       console.log('CLICOU PRODUTOS');
 
-    await page.waitForTimeout(1000);        
+     await page.waitForTimeout(2000);
+  
+     const menuTresPontos = page.locator('table tr:first-child >> text=more_vert');
 
-    await page.waitForSelector('table', { state: 'visible' });
-    const menuTresPontos = page.locator('table tr:first-child >> text=more_vert').first();    
-    await menuTresPontos.click();    
+     if (await menuTresPontos.count() > 0 && await menuTresPontos.isVisible()) {
+      console.log('LOCALIZOU OS TRÊS PONTOS');
+      await menuTresPontos.click();
+      console.log('CLICOU NOS TRÊS PONTOS');
 
-    await page.waitForTimeout(1000);    
-    await page.waitForSelector('text=Excluir', { state: 'visible' });
-    await page.locator('text=Excluir').click();
-    console.log('CLICOU EM EXCLUIR');    
-    await page.waitForTimeout(2000);        
-        
-    await capturarRequisicoesApi(page);   
-    
-    await page.waitForTimeout(5000);    
+      await page.waitForTimeout(1000);
+      await page.waitForSelector('text=Excluir', { state: 'visible' });
+      await page.locator('text=Excluir').click();
+      console.log('CLICOU EM EXCLUIR');
+
+      await page.waitForTimeout(2000);
+      await capturarRequisicoesApi(page);
+      await page.waitForTimeout(4000);
+     } else {
+      console.log('NENHUM REGISTRO ENCONTRADO NA GRADE, NADA PARA EXCLUIR.');
+     }
 });
