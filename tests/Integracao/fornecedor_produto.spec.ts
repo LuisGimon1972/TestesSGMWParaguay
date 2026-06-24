9305f90f4678a9c7c2e012852e15ece56ffee66e
Luis Manuel <luismanuel@SGBR.intra>
2026-06-24 15:14:39 -0300

    Adicionar testes integracao fornecedor produtos 2

--- tests/Integracao/fornecedor_produto.spec.ts
+++ tests/Integracao/fornecedor_produto.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test';
 import { loginCompleto } from '../../utils/loginCompleto';
 import { capturarRequisicoesApi } from '../../utils/capturaApi';
 
-test('Cadastro de Fornecedores', async ({ page }) => {
+test('Teste de Integração Fornecedor e Produtos', async ({ page }) => {
     await page.setViewportSize({ width: 1920, height: 1080 });
     await loginCompleto(page);    
 
