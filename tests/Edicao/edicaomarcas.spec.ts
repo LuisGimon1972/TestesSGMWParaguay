13360e0bc457f5ec42b9203f0c77e3b0dd14e3c5
Luis Manuel <luismanuel@SGBR.intra>
2026-06-29 11:59:58 -0300

    Ajustar edicao MARCAS

--- tests/Edicao/edicaomarcas.spec.ts
+++ tests/Edicao/edicaomarcas.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { loginCompleto } from '../../utils/loginCompleto';
 import { capturarRequisicoesApi } from '../../utils/capturaApi';
+import { capturarRequisicaoApiCadastro } from '../../utils/capturaApipayload';
 
 test('Edição de datos marcas', async ({ page }) => {
     await page.setViewportSize({ width: 1920, height: 1080 });
@@ -26,14 +27,19 @@ test('Edição de datos marcas', async ({ page }) => {
         await page.locator('table img[src="/icons/edit.svg"]').first().click();
         console.log('CLICOU NO ÍCONE DE EDITAR');    
 
+        console.log('***DADOS ENVIADOS PRA API***');  
+        await page.waitForTimeout(2000);
         const marca = `TEST MARCA ALTERADA ${Date.now()}`;
         await page.getByLabel(/editar marca/i).fill(marca);
         console.log('NOME DE MARCA ALTERADA OK:', marca);           
+        console.log('***FIM DE DADOS ENVIADOS***');  
 
         await page.locator('.q-btn')
         .filter({ hasText: /confirmar|guardar/i })
         .click({ force: true });
         console.log('CLICOU EM SALVAR MARCA');  
+
+        await capturarRequisicaoApiCadastro(page, '/api/marca');  
     }
     else{
         console.log('NENHUM REGISTRO ENCONTRADO NA GRADE, NADA PARA EDITAR.');  
