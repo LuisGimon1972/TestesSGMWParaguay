2943ef498fbb185deebac93c4f61b1321adfed50
Luis Manuel <luismanuel@SGBR.intra>
2026-07-03 14:44:31 -0300

    Ajustar ediçao pessoas para fazer um Get apos de salvar

--- tests/Cadastros/faturamento.spec.ts
+++ tests/Cadastros/faturamento.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from '@playwright/test';
 import { loginCompleto } from '../../utils/loginCompleto';
 import { capturarRequisicoesApi } from '../../utils/capturaApi';
+import { capturarRequisicaoApiCadastro } from '../../utils/capturaApipayload';
 
-test('Teste de Integração Cliente e Faturamento', async ({ page }) => {
+test('Teste de Cadastro de Faturas', async ({ page }) => {
     await page.setViewportSize({ width: 1920, height: 1080 });
     await loginCompleto(page);            
     
@@ -38,6 +39,7 @@ test('Teste de Integração Cliente e Faturamento', async ({ page }) => {
     await page.waitForTimeout(2000);
     await page.locator('.q-select').nth(5).click();
     await page.locator('(//div[contains(@class,"q-menu")]//*[contains(@class,"q-item")])[1]').click();
+    console.log('SELECIONOU UM DESTINATÁRIO/REMITENTE OK');  
 
     await page.waitForTimeout(4000);
 
@@ -46,41 +48,66 @@ test('Teste de Integração Cliente e Faturamento', async ({ page }) => {
     ).first();
     await botaoItens.waitFor({ state: 'visible' });
     await botaoItens.click({ force: true });
-
+    console.log('CLICOU EM ITEM DA FATURA OK');  
+    
     await page.getByText('Seleção de produto(s)').waitFor({ state: 'visible' });
     const ativos = page.getByText('Ativo', { exact: true });
     await ativos.nth(0).click({ force: true });
     await ativos.nth(1).click({ force: true });
     await ativos.nth(2).click({ force: true });
     await ativos.nth(3).click({ force: true });
+    console.log('SELECIONOU VÁRIOS ITENS DA FATURA OK');  
 
     await page.waitForTimeout(3000);
 
     await page.locator('.q-btn')
-      .filter({ hasText: /adicionar/i })
-      .click({ force: true });
-      console.log('CLICOU EM ADICIONAR');  
+    .filter({ hasText: /adicionar/i })
+    .click({ force: true });
+    console.log('CLICOU EM ADICIONAR');  
 
     await page.waitForTimeout(1000);
 
-        await page.locator('.q-btn')
-      .filter({ hasText: /salvar/i })
-      .click({ force: true });
-      console.log('CLICOU EM SALVAR');  
+    const finalizar = page
+    .locator('button.q-btn')
+    .filter({ hasText: 'FINALIZAR' });
+    await finalizar.first().waitFor({ state: 'visible' });
+    await finalizar.first().click({ force: true }); 
 
-    await page.waitForTimeout(5000);
-    
-    
+    const saldoTexto = await page
+    .locator('text=Saldo venda')
+    .locator('xpath=following::*[contains(text(),"Gs")][1]')
+    .innerText();
 
-function gerarRUC() {
-  const base = Math.floor(1000000 + Math.random() * 9000000).toString(); // 7 dígitos
-  const pesos = [2,3,4,5,6,7,2];
-  let soma = 0;
-  for (let i = 0; i < base.length; i++) {
-    soma += parseInt(base[i]) * pesos[i];
-  }
-  const resto = soma % 11;
-  const dv = resto > 1 ? 11 - resto : 0;
-  return `${base}-${dv}`;
-}
+    const saldo = Number(
+      saldoTexto
+        .replace('Gs', '')
+        .trim()
+        .replace(/\./g, '')
+        .replace(',', '.')
+    );
+    console.log(saldo); // 3686       
+
+    const valorMaior = Math.ceil(saldo + 10);
+    const efectivoTexto = page.getByText('EFECTIVO', { exact: true });
+    await efectivoTexto.waitFor({ state: 'visible' });
+    const pos = await efectivoTexto.evaluate((el) => {
+      const r = el.getBoundingClientRect();
+      return {
+        x: r.left + r.width / 2,
+        y: r.top + r.height / 2,
+      };
+    });
+    await page.mouse.click(pos.x, pos.y);
+    await page.locator('input:visible').last().fill(String(valorMaior));   
+
+    const confirmar = page
+    .locator('button.q-btn')
+    .filter({ hasText: 'CONFIRMAR' });
+    await confirmar.first().waitFor({ state: 'visible' });
+    await confirmar.first().click({ force: true }); 
+
+    await capturarRequisicaoApiCadastro(page, '/api/py/venda'); 
+     
+    await capturarRequisicoesApi(page); 
+    await page.waitForTimeout(4000);         
 });
--- tests/Edicao/edicaopessoas.spec.ts
+++ tests/Edicao/edicaopessoas.spec.ts
@@ -1,82 +1,155 @@
 import { test } from '@playwright/test';
 import { loginCompleto } from '../../utils/loginCompleto';
 import { capturarRequisicoesApi } from '../../utils/capturaApi';
-import { capturarRequisicaoApiCadastro } from '../../utils/capturaApipayload';
 
 test('Edição de datos Pessoas', async ({ page }) => {
-    await page.setViewportSize({ width: 1920, height: 1080 });
-    await loginCompleto(page);    
+  test.setTimeout(120000);
 
-    await page.waitForTimeout(1000);
-    await page.getByText(/pessoas/i).click({ force: true }); 
-    console.log('CLICOU PESSOAS');  
-    
-    await page.waitForTimeout(2000);
-        
-    await page.waitForSelector('table');
-    await page.locator('.q-skeleton').first().waitFor({ state: 'detached', timeout: 15000 });    
-    const editIcons = await page.locator('table img[src*="edit"], table svg').count();
-    console.log('Quantidade de ícones de edição:', editIcons);
+  await page.setViewportSize({ width: 1920, height: 1080 });
+  await loginCompleto(page);
 
-    if (editIcons > 0) {        
-      await page.locator('table img[src="/icons/edit.svg"]').first().click();
-      console.log('CLICOU NO ÍCONE DE EDITAR');    
-     
-      console.log('***DADOS ENVIADOS PRA API**'); 
-      await page.waitForTimeout(1000);
-      await page.locator('[aria-label="Tipo de operação"]').click({ force: true });
-      const menuDoc1 = page.locator('.q-menu').last();
-      await menuDoc1.waitFor();
-      await menuDoc1
-      .locator('.q-item')
-      .filter({ hasText: /B2F/i })
-      .click({ force: true }); 
-      const tipoop = await page.locator('input[aria-label="Tipo de operação"]').inputValue();      
-      console.log('TIPO DE OPERAÇÃO OK:', tipoop );
+  await page.waitForTimeout(1000);
+  await page.getByText(/pessoas/i).click({ force: true });
+  console.log('CLICOU PESSOAS');
 
-      await page.waitForTimeout(1000);
-      const nome = `TEST CLIENTE ALTERADO ${Date.now()}`;
-      await page.getByLabel(/nome completo/i).fill(nome);
-      console.log('NOMBRE DO CLIENTE ALTERADO OK:', nome);
+  await page.waitForTimeout(2000);
+  await page.waitForSelector('table');
+  await page.locator('.q-skeleton').first().waitFor({ state: 'detached', timeout: 15000 });
 
-      await page.waitForTimeout(1000);
-      const direccion = `TEST DIRECCION ALTERADA ${Date.now()}`;
-      await page.getByLabel(/direção/i).fill(direccion);
-      console.log('ENDEREÇO ALTERADO OK:', direccion);
+  const editIcons = await page.locator('table img[src="/icons/edit.svg"]').count();
+  console.log('Quantidade de ícones de edição:', editIcons);
 
-      await page.waitForTimeout(1000);
-      const numero = Math.floor(Math.random() * 4000) + 1;
-      const campoNumero = page.locator('.q-field')
-      .filter({ hasText: /número/i })
-      .last();
-      await campoNumero.locator('input').fill(numero.toString());
-      console.log('NÚMERO ALTERADO OK:', numero.toFixed(0));
-
-      await page.waitForTimeout(1000);
-      const telefone = Array.from({ length: 9 }, () =>
-        Math.floor(Math.random() * 10)
-      ).join('');      
-      const inputTelefone = page.locator('input[type="tel"]').first();
-      await inputTelefone.scrollIntoViewIfNeeded();
-      await inputTelefone.click({ force: true });
-      await inputTelefone.press('Control+A');
-      await inputTelefone.press('Backspace');
-      await inputTelefone.type(telefone, { delay: 30 });
-      console.log('TELEFONE ALTERADO OK:', telefone);    
-      console.log('***FIM DE DADOS ENVIADOS**'); 
-
-      await page.locator('.q-btn')
-      .filter({ hasText: /salvar|guardar/i })
-      .click({ force: true });
-      console.log('CLICOU EM SALVAR');           
-
-      await capturarRequisicaoApiCadastro(page, '/api/py/pessoa'); 
-      
-      await capturarRequisicoesApi(page); 
-      await page.waitForTimeout(4000);    
-  }
-  else  {
-      console.log('NENHUM REGISTRO ENCONTRADO NA GRADE, NADA PARA EDITAR.');  
+  if (editIcons === 0) {
+    console.log('NENHUM REGISTRO ENCONTRADO NA GRADE, NADA PARA EDITAR.');
+    return;
   }
 
+  const getRegistroEditadoPromise = page.waitForResponse((response) =>
+    response.url().includes('/api/py/pessoa') &&
+    response.request().method() === 'GET' &&
+    response.status() === 200 &&
+    /\/api\/py\/pessoa\/[^/?]+/.test(response.url())
+  );
+
+  //Captura Dados Antes de Alterar
+  const getPessoaPromise = page.waitForResponse((response) =>
+    response.url().includes('/api/py/pessoa') &&
+    response.request().method() === 'GET' &&
+    response.status() === 200
+  );
+  //Captura Dados Antes de Alterar
+
+  await page.locator('table img[src="/icons/edit.svg"]').first().click();
+  console.log('CLICOU NO ÍCONE DE EDITAR');
+
+  //Mostra Dados Antes de Alterar
+  const getPessoaResponsee = await getPessoaPromise;
+  const dadosAntes = await getPessoaResponsee.json();
+  console.log('***DADOS ANTES DA ALTERAÇÃO***');
+  console.log(JSON.stringify(dadosAntes, null, 2));
+  //Mostra Dados Antes de Alterar  
+
+  const getRegistroEditadoResponse = await getRegistroEditadoPromise;
+  const urlRegistroEditado = getRegistroEditadoResponse.url();
+  const headersOriginais = getRegistroEditadoResponse.request().headers();
+
+  console.log('URL DO REGISTRO EDITADO:', urlRegistroEditado);
+
+  console.log('***DADOS ENVIADOS PRA API**');
+  await page.waitForTimeout(1000);
+  await page.locator('[aria-label="Tipo de operação"]').click({ force: true });
+  const menuDoc1 = page.locator('.q-menu').last();
+  await menuDoc1.waitFor();
+  await menuDoc1
+    .locator('.q-item')
+    .filter({ hasText: /B2F/i })
+    .click({ force: true });
+  const tipoop = await page.locator('input[aria-label="Tipo de operação"]').inputValue();
+  console.log('TIPO DE OPERAÇÃO OK:', tipoop);
+
+  await page.waitForTimeout(1000);
+  const nome = `TEST CLIENTE ALTERADO ${Date.now()}`;
+  await page.getByLabel(/nome completo/i).fill(nome);
+  console.log('NOMBRE DO CLIENTE ALTERADO OK:', nome);
+
+  await page.waitForTimeout(1000);
+  const direccion = `TEST DIRECCION ALTERADA ${Date.now()}`;
+  await page.getByLabel(/direção/i).fill(direccion);
+  console.log('ENDEREÇO ALTERADO OK:', direccion);
+
+  await page.waitForTimeout(1000);
+  const numero = Math.floor(Math.random() * 4000) + 1;
+  const campoNumero = page.locator('.q-field')
+    .filter({ hasText: /número/i })
+    .last();
+  await campoNumero.locator('input').fill(numero.toString());
+  console.log('NÚMERO ALTERADO OK:', numero.toFixed(0));
+
+  await page.waitForTimeout(1000);
+  const telefone = Array.from({ length: 9 }, () =>
+    Math.floor(Math.random() * 10)
+  ).join('');
+  const inputTelefone = page.locator('input[type="tel"]').first();
+  await inputTelefone.scrollIntoViewIfNeeded();
+  await inputTelefone.click({ force: true });
+  await inputTelefone.press('Control+A');
+  await inputTelefone.press('Backspace');
+  await inputTelefone.type(telefone, { delay: 30 });
+  console.log('TELEFONE ALTERADO OK:', telefone);
+  console.log('***FIM DE DADOS ENVIADOS**');
+
+  const salvarPessoaPromise = page.waitForResponse((response) =>
+    response.url().includes('/api/py/pessoa') &&
+    ['PUT', 'PATCH', 'POST'].includes(response.request().method()) &&
+    response.status() >= 200 &&
+    response.status() < 300
+  );
+
+  await page.locator('.q-btn')
+    .filter({ hasText: /salvar|guardar/i })
+    .click({ force: true });
+  console.log('CLICOU EM SALVAR');
+
+  await salvarPessoaPromise;
+
+  const headersGetRegistro: Record<string, string> = {
+    Accept: 'application/json',
+    'X-Requested-With': 'XMLHttpRequest',
+  };
+
+  if (headersOriginais.authorization) {
+    headersGetRegistro.authorization = headersOriginais.authorization;
+  }
+
+  if (headersOriginais['x-xsrf-token']) {
+    headersGetRegistro['x-xsrf-token'] = headersOriginais['x-xsrf-token'];
+  }
+
+  if (headersOriginais['x-tenant']) {
+    headersGetRegistro['x-tenant'] = headersOriginais['x-tenant'];
+  }
+
+  if (headersOriginais['x-empresa']) {
+    headersGetRegistro['x-empresa'] = headersOriginais['x-empresa'];
+  }
+
+  const getPessoaResponse = await page.request.get(urlRegistroEditado, {
+    headers: headersGetRegistro,
+  });
+
+  console.log('STATUS GET REGISTRO EDITADO:', getPessoaResponse.status());
+
+  const textoResposta = await getPessoaResponse.text();
+
+  if (!getPessoaResponse.ok()) {
+    throw new Error(`GET registro editado falhou: ${getPessoaResponse.status()} - ${textoResposta}`);
+  }
+
+  const dadosDepois = JSON.parse(textoResposta);
+
+  console.log('***DADOS APÓS DA ALTERAÇÃO (GET DO REGISTRO EDITADO)***');
+  console.log(JSON.stringify(dadosDepois, null, 2));
+
+  await capturarRequisicoesApi(page); 
+  await page.waitForTimeout(4000);      
 });
