8d2f9a251ea87c5022c58432fad56bc56f671b3f
Luis Manuel <luismanuel@SGBR.intra>
2026-07-06 16:31:47 -0300

    Adicionar exclusao de usuarios Get apos de apagar

--- tests/Exclusao/exclusaousuarios.spec.ts
+++ tests/Exclusao/exclusaousuarios.spec.ts
@@ -2,26 +2,30 @@ import { test, expect } from '@playwright/test';
 import { loginCompleto } from '../../utils/loginCompleto';
 import { capturarRequisicoesApi } from '../../utils/capturaApi';
 
-test('Exclusão de datos Produtos', async ({ page }) => {
-    await page.setViewportSize({ width: 1920, height: 1080 });
-    await loginCompleto(page);    
+test('Exclusão de datos funcionários', async ({ page }) => {
+  await page.setViewportSize({ width: 1920, height: 1080 });
 
-    await page.waitForTimeout(1000);
-      await Promise.all([
-      page.waitForURL(/producto/, { timeout: 15000 }),
-      page.locator('a[href*="producto"]').first().click()
-      ]);
-      console.log('CLICOU PRODUTOS');
-
-     await page.waitForTimeout(2000);
+  await loginCompleto(page);
   
-     const menuTresPontos = page.locator('table tr:first-child >> text=more_vert');
+  const usuariosBtn = page.getByText(/usu[aá]rios/i).first();
+  await expect(usuariosBtn).toBeVisible();
+  await usuariosBtn.click();
+  console.log('CLICOU EM USUÁRIOS');
 
-     if (await menuTresPontos.count() > 0 && await menuTresPontos.isVisible()) {
-      console.log('LOCALIZOU OS TRÊS PONTOS');
-      await menuTresPontos.click();
-      console.log('CLICOU NOS TRÊS PONTOS');
+  const listado = page.locator('a[href*="usuario/listado"]');
+  await expect(listado).toBeVisible();
+  await listado.click();
+  console.log('CLICOU EM LISTAGEM DE USUARIOS');
+  
+  await page.waitForTimeout(2000);
+  
+  const trashIcons = await page.locator('table img[src*="trash"]').count();
+  console.log('Quantidade de ícones de edição:', trashIcons);
 
+    if (trashIcons === 0) {
+       console.log('NENHUM ÍCONE DE EXCLUSÃO ENCONTRADO NA GRADE, NADA PARA EDITAR.');
+       return;
+    } 
       const primeiraLinha = page.locator('table tr:first-child td');
       const qtdColunas = await primeiraLinha.count();
 
@@ -30,11 +34,11 @@ test('Exclusão de datos Produtos', async ({ page }) => {
         const valor = await primeiraLinha.nth(i).textContent();
         console.log(`Coluna ${i}: ${valor?.trim()}`);
       }
-     
-      await page.waitForSelector('table tr:first-child td', { state: 'visible' });
 
-      const codigoProduto = await primeiraLinha.nth(2).textContent(); // exemplo: coluna 1
-      const codigoLimpo = codigoProduto?.trim();
+      await page.waitForSelector('table tr:first-child td', { state: 'visible' });
+      
+      const codigoPessoa = await primeiraLinha.nth(2).textContent(); // exemplo: coluna 1
+      const codigoLimpo = codigoPessoa?.trim();
 
       if (!codigoLimpo) {
         throw new Error('⚠️ Não foi possível capturar o código da pessoa na tabela.');
@@ -42,21 +46,20 @@ test('Exclusão de datos Produtos', async ({ page }) => {
       console.log(`CÓDIGO SELECIONADO: ${codigoLimpo}`);
 
       await page.waitForTimeout(1000);
-      await page.waitForSelector('text=Excluir', { state: 'visible' });
-      await page.locator('text=Excluir').click();      
-      console.log('CLICOU EM EXCLUIR');   
-
+      await page.locator('table img[src*="trash"]').first().click();
+      console.log('CLICOU EM EXCLUIR'); 
+      
       await page.waitForTimeout(1000);
       await page.waitForSelector('button:has-text("EXCLUIR")');
       await page.click('button:has-text("EXCLUIR")');
       console.log('CLICOU EM EXCLUIR NO DIÁLOGO DE CONFIRMAÇÃO');
 
       const deleteResponse = await page.waitForResponse((response) =>
-      response.url().includes(`/api/py/produto/geral/${codigoLimpo}`) &&
+      response.url().includes(`/api/usuario/${codigoLimpo}`) &&
       response.request().method() === 'DELETE');
       expect([200, 204]).toContain(deleteResponse.status());
 
-      const getExcluidoResponse = await page.request.get(`/api/py/produto/geral/${codigoLimpo}`);
+      const getExcluidoResponse = await page.request.get(`/api/usuario/${codigoLimpo}`);
 
       console.log('***RESPOSTA DA API AO CONSULTAR REGISTRO EXCLUÍDO***');
       console.log(`Status: ${getExcluidoResponse.status()}`);
@@ -70,12 +73,11 @@ test('Exclusão de datos Produtos', async ({ page }) => {
       
       expect([404, 200]).toContain(getExcluidoResponse.status());
 
-      console.log(`Registro ${codigoLimpo} removido com sucesso.`); 
+      console.log(`Registro ${codigoLimpo} removido com sucesso.`);           
+      
 
       await page.waitForTimeout(2000);
       await capturarRequisicoesApi(page);
       await page.waitForTimeout(4000);
-     } else {
-      console.log('NENHUM REGISTRO ENCONTRADO NA GRADE, NADA PARA EXCLUIR.');
-     }
+
 });
