0bd1e18054f639a7acd68b9e0b9aa9e6a763953a
Luis Manuel <luismanuel@SGBR.intra>
2026-07-06 17:13:33 -0300

    Ajustar exclusao de grupos Get apos de apagar 5

--- tests/Exclusao/exclusaogrupos.spec.ts
+++ tests/Exclusao/exclusaogrupos.spec.ts
@@ -17,14 +17,62 @@ test('Exclusão de datos grupos', async ({ page }) => {
     console.log('CLICOU EM GRUPOS');
 
     await page.waitForTimeout(2000);
-    const trashIcons = await page.locator('table img[src*="trash"]').count();
-    if (trashIcons > 0) {
-        await page.locator('table img[src*="trash"]').first().click();
-        await capturarRequisicaoApiDelete(page, '/api/produto/grupo');  
+     const trashIcons = await page.locator('table img[src*="trash"]').count();
+  console.log('Quantidade de ícones de edição:', trashIcons);
 
-        await capturarRequisicoesApi(page);
-        await page.waitForTimeout(4000);
-    } else {
-        console.log('NENHUM REGISTRO ENCONTRADO NA GRADE, NADA PARA EXCLUIR.');
+    if (trashIcons === 0) {
+       console.log('NENHUM ÍCONE DE EXCLUSÃO ENCONTRADO NA GRADE, NADA PARA EDITAR.');
+       return;
+    } 
+    const primeiraLinha = page.locator('table tr:first-child td');
+    const qtdColunas = await primeiraLinha.count();
+
+    console.log('CAPTURA DO REGISTRO DA GRADE ANTES DE SER REMOVIDO:');
+    for (let i = 1; i < qtdColunas; i++) {
+      const valor = await primeiraLinha.nth(i).textContent();
+      console.log(`Coluna ${i}: ${valor?.trim()}`);
     }
+    
+          await page.waitForSelector('table tr:first-child td', { state: 'visible' });
+      
+      const codigoPessoa = await primeiraLinha.nth(2).textContent(); // exemplo: coluna 1
+      const codigoLimpo = codigoPessoa?.trim();
+
+      if (!codigoLimpo) {
+        throw new Error('⚠️ Não foi possível capturar o código da pessoa na tabela.');
+      }
+      console.log(`CÓDIGO SELECIONADO: ${codigoLimpo}`);
+
+      await page.waitForTimeout(1000);
+      await page.locator('table img[src*="trash"]').first().click();
+      console.log('CLICOU EM EXCLUIR'); 
+      
+      await page.waitForTimeout(1000);
+      await page.waitForSelector('button:has-text("EXCLUIR")');
+      await page.click('button:has-text("EXCLUIR")');
+      console.log('CLICOU EM EXCLUIR NO DIÁLOGO DE CONFIRMAÇÃO');
+
+      const deleteResponse = await page.waitForResponse((response) =>
+      response.url().includes(`/api/produto/grupo/${codigoLimpo}`) &&
+      response.request().method() === 'DELETE');
+      expect([200, 204]).toContain(deleteResponse.status());
+
+      const getExcluidoResponse = await page.request.get(`/api/produto/grupo/${codigoLimpo}`);
+
+      console.log('***RESPOSTA DA API AO CONSULTAR REGISTRO EXCLUÍDO***');
+      console.log(`Status: ${getExcluidoResponse.status()}`);
+
+      try {
+        const dadosExcluido = await getExcluidoResponse.json();
+        console.log(JSON.stringify(dadosExcluido, null, 2));
+      } catch {
+        console.log('Resposta sem corpo. (Status Code: 404)');
+      }
+      
+      expect([404, 200]).toContain(getExcluidoResponse.status());
+
+      console.log(`Registro ${codigoLimpo} removido com sucesso.`);           
+
+    await capturarRequisicoesApi(page);
+    await page.waitForTimeout(4000);    
 });
