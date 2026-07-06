9f8612ea4408bf5dd097fae0cca5c3b6de8bce8a
Luis Manuel <luismanuel@SGBR.intra>
2026-07-06 17:28:04 -0300

    Ajustar exclusao Get apos de apagar 2

--- tests/Exclusao/exclusaogrupos.spec.ts
+++ tests/Exclusao/exclusaogrupos.spec.ts
@@ -1,7 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { loginCompleto } from '../../utils/loginCompleto';
 import { capturarRequisicoesApi } from '../../utils/capturaApi';
-import { capturarRequisicaoApiDelete } from '../../utils/capturaApidelete';
 
 test('Exclusão de datos grupos', async ({ page }) => {
     await page.setViewportSize({ width: 1920, height: 1080 });
