5ee70a65b1d267f491f4f4ed213252e4d634cdde
Luis Manuel <luismanuel@SGBR.intra>
2026-07-08 14:49:11 -0300

    Aplicar get apos do cadastro de Espécies

--- tests/Cadastros/especies.spec.ts
+++ tests/Cadastros/especies.spec.ts
@@ -6,6 +6,14 @@ import { capturarRequisicaoApiCadastro } from '../../utils/capturaApipayload';
 test('Cadastro de espécies', async ({ page }) => {
     await page.setViewportSize({ width: 1920, height: 1080 });    
     await loginCompleto(page);    
+
+    await page.waitForTimeout(2000);       
+    
+    const salvarEspeciePromise = page.waitForResponse((response) =>
+    response.url().includes('/api/especie') &&
+    ['POST'].includes(response.request().method()) &&
+    response.status() >= 200 &&
+    response.status() < 300);
  
     await page.waitForTimeout(1000);
     await page.getByText(/cadastros/i).click({ force: true }); 
@@ -70,7 +78,42 @@ test('Cadastro de espécies', async ({ page }) => {
     .click({ force: true });
     console.log('CLICOU EM SALVAR');  
 
-    await capturarRequisicaoApiCadastro(page, '/api/especie');  
+    const salvarPessoaResponse = await salvarEspeciePromise;
+    const dadosSalvos = await salvarPessoaResponse.json();
+    console.log('***DADOS RETORNADOS NA CRIAÇÃO***');
+    console.log(JSON.stringify(dadosSalvos, null, 2));
+    
+    const idEspecie = dadosSalvos.controle.toString().trim();
+    console.log('CONTROLE:', idEspecie);    
+    const urlRegistroCriado = `https://testepyeduardo.global-hom.sgmw.com.br/api/especie/${idEspecie}`;    
+    const headersOriginais = salvarPessoaResponse.request().headers();
+    const headersGetRegistro: Record<string, string> = {
+      Accept: 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
+      authorization: headersOriginais['authorization'],
+      'x-xsrf-token': headersOriginais['x-xsrf-token'],
+      'x-tenant': headersOriginais['x-tenant'],
+      'x-empresa': headersOriginais['x-empresa'],
+    };
+    
+    const getCriadoResponse = await page.request.get(urlRegistroCriado, {
+      headers: headersGetRegistro,
+    });
+
+    console.log('***RESPOSTA DA API AO CONSULTAR O NOVO REGISTRO***');
+    console.log(`Status: ${getCriadoResponse.status()}`);
+
+    try {
+      const dadosCriado = await getCriadoResponse.json();
+      console.log(JSON.stringify(dadosCriado, null, 2));
+    } catch (error) {
+      console.error('Erro ao converter resposta para JSON:', error);
+      const corpoBruto = await getCriadoResponse.text();
+      console.log('Corpo bruto da resposta:', corpoBruto);
+    }
+
+    expect([404, 200]).toContain(getCriadoResponse.status());    
+    
     
     await capturarRequisicoesApi(page); 
     await page.waitForTimeout(4000);    
