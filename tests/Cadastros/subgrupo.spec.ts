5b941d152a118892aa92dbf2b6ccbba9af5609b0
Luis Manuel <luismanuel@SGBR.intra>
2026-07-08 15:00:07 -0300

    Aplicar get apos do cadastro de SubGrupos

--- tests/Cadastros/subgrupo.spec.ts
+++ tests/Cadastros/subgrupo.spec.ts
@@ -1,11 +1,17 @@
 import { test, expect } from '@playwright/test';
 import { loginCompleto } from '../../utils/loginCompleto';
 import { capturarRequisicoesApi } from '../../utils/capturaApi';
-import { capturarRequisicaoApiCadastro } from '../../utils/capturaApipayload';
 
 test('Cadastro de subgrupos', async ({ page }) => {
     await page.setViewportSize({ width: 1920, height: 1080 });
     await loginCompleto(page);    
+
+    await page.waitForTimeout(2000);           
+    const salvarSubgrupoPromise = page.waitForResponse((response) =>
+    response.url().includes('/api/produto/subgrupo') &&
+    ['POST'].includes(response.request().method()) &&
+    response.status() >= 200 &&
+    response.status() < 300);
  
     const cadBtn = page.getByText(/cadastros/i).first();
     await expect(cadBtn).toBeVisible();
@@ -47,7 +53,41 @@ test('Cadastro de subgrupos', async ({ page }) => {
     .click({ force: true });
     console.log('CLICOU EM SALVAR SUBGRUPO');  
 
-    await capturarRequisicaoApiCadastro(page, '/api/produto/subgrupo');  
+    const salvarPessoaResponse = await salvarSubgrupoPromise;
+    const dadosSalvos = await salvarPessoaResponse.json();
+    console.log('***DADOS RETORNADOS NA CRIAÇÃO***');
+    console.log(JSON.stringify(dadosSalvos, null, 2));
+    
+    const idSubgrupo = dadosSalvos.controle.toString().trim();
+    console.log('CONTROLE:', idSubgrupo);    
+    const urlRegistroCriado = `https://testepyeduardo.global-hom.sgmw.com.br/api/produto/subgrupo/${idSubgrupo}`;    
+    const headersOriginais = salvarPessoaResponse.request().headers();
+    const headersGetRegistro: Record<string, string> = {
+      Accept: 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
+      authorization: headersOriginais['authorization'],
+      'x-xsrf-token': headersOriginais['x-xsrf-token'],
+      'x-tenant': headersOriginais['x-tenant'],
+      'x-empresa': headersOriginais['x-empresa'],
+    };
+    
+    const getCriadoResponse = await page.request.get(urlRegistroCriado, {
+      headers: headersGetRegistro,
+    });
+
+    console.log('***RESPOSTA DA API AO CONSULTAR O NOVO REGISTRO***');
+    console.log(`Status: ${getCriadoResponse.status()}`);
+
+    try {
+      const dadosCriado = await getCriadoResponse.json();
+      console.log(JSON.stringify(dadosCriado, null, 2));
+    } catch (error) {
+      console.error('Erro ao converter resposta para JSON:', error);
+      const corpoBruto = await getCriadoResponse.text();
+      console.log('Corpo bruto da resposta:', corpoBruto);
+    }
+
+    expect([404, 200]).toContain(getCriadoResponse.status());    
     
     await capturarRequisicoesApi(page); 
     await page.waitForTimeout(4000);   
