d523ae5d4f4443c3fda85a764c5b770ccfe23baf
Luis Manuel <luismanuel@SGBR.intra>
2026-07-10 09:21:47 -0300

    Ajuste do cadastro cotacao 2

--- tests/Cadastros/cotacao.spec.ts
+++ tests/Cadastros/cotacao.spec.ts
@@ -5,6 +5,12 @@ import { capturarRequisicoesApi } from '../../utils/capturaApi';
 test('Cadastro de cotação de moedas', async ({ page }) => {
     await page.setViewportSize({ width: 1920, height: 1080 });
     await loginCompleto(page);    
+
+    const salvarCotacaoPromise = page.waitForResponse((response) =>
+    response.url().includes('/api/moeda/cotacao') &&
+    ['POST'].includes(response.request().method()) &&
+    response.status() >= 200 &&
+    response.status() < 300);
  
     const cadBtn = page.getByText(/cadastros/i).first();
     await expect(cadBtn).toBeVisible();
@@ -39,13 +45,13 @@ test('Cadastro de cotação de moedas', async ({ page }) => {
     const inputVenta = page.getByLabel(/valor de venda/i);
     await expect(inputVenta).toBeVisible();
     await inputVenta.fill(String(venta));
-    console.log('VALOR DE VENTA OK', venta);
+    console.log('VALOR DE VENTA OK', venta.toString().trim());
 
     const compra = Math.floor(Math.random() * (5000 - 4500 + 1)) + 4500;
     const inputCompra = page.getByLabel(/valor de compra/i);
     await expect(inputCompra).toBeVisible();
     await inputCompra.fill(String(compra));
-    console.log('VALOR DE COMPRA OK', compra);
+    console.log('VALOR DE COMPRA OK:', compra.toString().trim());
 
     const hoje = new Date();
     const datahoje = hoje.toLocaleDateString('pt-BR');
@@ -56,7 +62,7 @@ test('Cadastro de cotação de moedas', async ({ page }) => {
     .locator('input');
     await expect(inputData).toBeVisible();
     await inputData.fill(datahoje);
-    console.log('INICIO DE VIGÊNCIA OK', datahoje);
+    console.log('INICIO DE VIGÊNCIA OK:', datahoje);
 
     await page.waitForTimeout(2000);    
     
@@ -75,12 +81,50 @@ test('Cadastro de cotação de moedas', async ({ page }) => {
     await expect(inputDatafin).toBeVisible();
     await inputDatafin.fill('');
     await inputDatafin.type(datafin, { delay: 50 });
-    console.log('FIM DE VIGÊNCIA OK', datafin);
+    console.log('FIM DE VIGÊNCIA OK:', datafin);
 
     await page.locator('.q-btn')
     .filter({ hasText: /salvar|guardar/i })
     .click({ force: true });
     console.log('CLICOU EM SALVAR COTACAO');  
+
+    const salvarPessoaResponse = await salvarCotacaoPromise;
+    const dadosSalvos = await salvarPessoaResponse.json();
+    console.log('***DADOS RETORNADOS NA CRIAÇÃO***');
+    console.log(JSON.stringify(dadosSalvos, null, 2));
+    
+    const idGrupo = dadosSalvos.controle.toString().trim();
+    console.log('CONTROLE:', idGrupo);    
+    const urlRegistroCriado = `https://testepyeduardo.global-hom.sgmw.com.br/api/moeda/cotacao/${idGrupo}`;    
+    const headersOriginais = salvarPessoaResponse.request().headers();
+    const headersGetRegistro: Record<string, string> = {
+      Accept: 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
+      authorization: headersOriginais['authorization'],
+      'x-xsrf-token': headersOriginais['x-xsrf-token'],
+      'x-tenant': headersOriginais['x-tenant'],
+      'x-empresa': headersOriginais['x-empresa'],
+    };
+    
+    const getCriadoResponse = await page.request.get(urlRegistroCriado, {
+      headers: headersGetRegistro,
+    });
+
+    console.log('***RESPOSTA DA API AO CONSULTAR O NOVO REGISTRO***');
+    console.log(`Status: ${getCriadoResponse.status()}`);
+
+    try {
+      const dadosCriado = await getCriadoResponse.json();
+      console.log(JSON.stringify(dadosCriado, null, 2));
+    } catch (error) {
+      console.error('Erro ao converter resposta para JSON:', error);
+      const corpoBruto = await getCriadoResponse.text();
+      console.log('Corpo bruto da resposta:', corpoBruto);
+    }
+
+    expect([404, 200]).toContain(getCriadoResponse.status());    
+
+
     
     await capturarRequisicoesApi(page); 
     await page.waitForTimeout(4000);    
