e069e546d99cdc2cb4907dbbe07bc6dd3561e05a
Luis Manuel <luismanuel@SGBR.intra>
2026-07-13 15:24:29 -0300

    Ajustar modulo finalizar venda

--- tests/Cadastros/finalizarfatura.spec.ts
+++ tests/Cadastros/finalizarfatura.spec.ts
@@ -19,16 +19,25 @@ test('Teste de Cadastro de Faturas', async ({ page }) => {
     ]);
     console.log('CLICOU EM FATURAMENTO');
 
+    await page.waitForSelector('button:has-text("Aberta")', { state: 'visible', timeout: 10000 });
+    await page.locator('button:has-text("Aberta")').click();
+    console.log('FILTROU FATURAS ABERTAS');
+
     await page.waitForTimeout(1000);
-    const btnCadastrar = page.getByText(/cadastrar fatura/i).first();
-    await btnCadastrar.waitFor();
-    await btnCadastrar.click({ force: true });
-    console.log('CLICOU EM CADASTRAR FATURA');
+    const editIcons = await page.locator('table img[src="/icons/edit.svg"]').count();
+    console.log('QUANTIDADE DE REGISTROS NA GRADE:', editIcons.toString().trim());
+
+    if (editIcons === 0) {
+        console.log('NENHUM REGISTRO ENCONTRADO NA GRADE, NADA PARA EDITAR.');
+        return;
+    }
+    await page.locator('table img[src="/icons/edit.svg"]').first().click();
+    console.log('CLICOU NO ÍCONE DE EDITAR');    
 
     await page.emulateMedia({ media: 'screen' });
-      await page.evaluate(() => {
-      document.body.style.zoom = '0.8'; });
-      console.log('🔍 Zoom ajustado para 80% via CSS');
+    await page.evaluate(() => {
+    document.body.style.zoom = '0.8'; });
+    console.log('🔍 Zoom ajustado para 80% via CSS');
    
     console.log('***DADOS ENVIADOS PRA API***');  
     await page.waitForTimeout(2000);
@@ -36,37 +45,14 @@ test('Teste de Cadastro de Faturas', async ({ page }) => {
     await page.locator('(//div[contains(@class,"q-menu")]//*[contains(@class,"q-item")])[1]').click();
     console.log('SELECIONOU UM DESTINATÁRIO/REMITENTE OK');  
 
-    await page.waitForTimeout(4000);
-
-    const botaoItens = page.locator(
-    'xpath=//button[.//i[normalize-space(.)="format_list_bulleted"]]'
-    ).first();
-    await botaoItens.waitFor({ state: 'visible' });
-    await botaoItens.click({ force: true });
-    console.log('CLICOU EM ITEM DA FATURA OK');  
-    
-    await page.getByText('Seleção de produto(s)').waitFor({ state: 'visible' });
-    const ativos = page.getByText('Ativo', { exact: true });
-    await ativos.nth(0).click({ force: true });
-    await ativos.nth(1).click({ force: true });
-    await ativos.nth(2).click({ force: true });
-    await ativos.nth(3).click({ force: true });
-    console.log('SELECIONOU VÁRIOS ITENS DA FATURA OK');  
-
-    await page.waitForTimeout(3000);
-
-    await page.locator('.q-btn')
-    .filter({ hasText: /adicionar/i })
-    .click({ force: true });
-    console.log('CLICOU EM ADICIONAR');  
-
-    await page.waitForTimeout(1000);
-
-     const finalizar = page
+    await page.waitForTimeout(2000);     
+    const finalizar = page
     .locator('button.q-btn')
     .filter({ hasText: 'FINALIZAR' });
     await finalizar.first().waitFor({ state: 'visible' });
     await finalizar.first().click({ force: true });   
+
+    await page.waitForTimeout(1000);     
 
      const saldoTexto = await page
     .locator('text=Saldo venda')
@@ -79,26 +65,23 @@ test('Teste de Cadastro de Faturas', async ({ page }) => {
         .trim()
         .replace(/\./g, '')
         .replace(',', '.')
-    );
-    console.log(saldo); // 3686       
+    )/100;
+    console.log('TOTAL DE VENDAS:',saldo); 
 
-    const valorMaior = Math.ceil(saldo + 10);
-    const efectivoTexto = page.getByText('EFECTIVO', { exact: true });
-    await efectivoTexto.waitFor({ state: 'visible' });
-    const pos = await efectivoTexto.evaluate((el) => {
-      const r = el.getBoundingClientRect();
-      return {
-        x: r.left + r.width / 2,
-        y: r.top + r.height / 2,
-      };
+    const valor = saldo;
+    const valorCorrigido = Math.floor(valor+2);
+    const troco = Math.floor(valor+2) - Math.floor(valor)
+    const efectivo = page.locator('.payment-specie-row', {
+      hasText: 'EFECTIVO'
     });
-    await page.mouse.click(pos.x, pos.y);
-    await page.locator('input:visible').last().fill(String(valorMaior));   
+    await efectivo.locator('input').fill(valorCorrigido.toString());
+    console.log('DIGITOU VALOR EM EFECTIVO:',valorCorrigido.toString()); 
+    console.log('CALCULOU TROCO:',troco.toString()); 
 
     const confirmar = page
     .locator('button.q-btn')
     .filter({ hasText: 'CONFIRMAR' });
     await confirmar.first().waitFor({ state: 'visible' });
-    await confirmar.first().click({ force: true });    
-       
+    await confirmar.first().click({ force: true });           
+    console.log('CLICLOU EM CONFIRMAR VENDA'); 
 });
