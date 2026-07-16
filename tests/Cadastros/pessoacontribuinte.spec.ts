6df959a330bae56b97d4968b08042a4f3f721515
Luis Manuel <luismanuel@SGBR.intra>
2026-07-16 09:26:23 -0300

    Adicionar cadastro de clientes contribuinte

---
+++ tests/Cadastros/pessoacontribuinte.spec.ts
@@ -0,0 +1,197 @@
+import { test, expect } from '@playwright/test';
+import { loginCompleto } from '../../utils/loginCompleto';
+import { capturarRequisicoesApi } from '../../utils/capturaApi';
+import { empresasParaguai } from '../../utils/rucs-paraguai';
+
+test('Cadastro de Clientes', async ({ page }) => {
+    await page.setViewportSize({ width: 1920, height: 1080 });
+    await loginCompleto(page);    
+
+    await page.waitForTimeout(2000);       
+    
+    const salvarPessoaPromise = page.waitForResponse((response) =>
+    response.url().includes('/api/py/pessoa') &&
+    ['POST'].includes(response.request().method()) &&
+    response.status() >= 200 &&
+    response.status() < 300
+  );
+
+    await page.waitForTimeout(1000);
+    await page.getByText(/pessoas/i).click({ force: true }); 
+    console.log(`✅ Clicou em pessoas`);  
+    console.log('CLICOU EM PESSOAS');    
+      
+    const btnCadastrar = page.getByText(/cadastrar pessoas/i).first();
+      await btnCadastrar.waitFor();
+      await btnCadastrar.click({ force: true });      
+      console.log(`✅ Clicou em Cadastrar Pessoas`);  
+
+    console.log('DADOS ENVIADOS PRA API');
+    await page.locator('[aria-label="Natureza"]').click({ force: true });
+    const menu = page.locator('.q-menu:visible');
+    await menu.waitFor();
+    await menu
+    .locator('.q-item')
+    .filter({ hasText: /contribuinte/i })
+    .first()
+    .click({ force: true });    
+    const natureza = await page.locator('input[aria-label="Natureza"]').inputValue();      
+    console.log('✅ Natureza:',natureza);
+
+    await page.locator('[aria-label="Tipo de contribuinte"]').click({ force: true });
+    const menuDoc = page.locator('.q-menu').last();
+    await menuDoc.waitFor();
+    await menuDoc
+    .locator('.q-item')
+    .filter({ hasText: /pessoa jurídica/i })
+    .click({ force: true });
+    const tipocon = await page.locator('input[aria-label="Tipo de contribuinte"]').inputValue();      
+    console.log('✅ Tipo de contribuinte:',tipocon);
+    
+    await page.waitForTimeout(700);
+    const ruc = gerarRUC();
+    await page.getByLabel(/RUC/i).fill(ruc);
+    console.log('✅ Número de idenficação RUC:', ruc);
+    
+    await page.waitForTimeout(700);
+    await page.locator('[aria-label="Tipo de operação"]').click({ force: true });
+    const menuDoc1 = page.locator('.q-menu').last();
+    await menuDoc1.waitFor();
+    await menuDoc1
+    .locator('.q-item')
+    .filter({ hasText: /B2B/i })
+    .click({ force: true }); 
+    const tipoop = await page.locator('input[aria-label="Tipo de operação"]').inputValue();      
+    console.log('✅ Tipo de Operação:', tipoop );
+
+    await page.locator('[aria-label="Tipo de cadastro"]').click({ force: true });
+    const menuDoc2 = page.locator('.q-menu').last();
+    await menuDoc2.waitFor();
+    await menuDoc2
+    .locator('.q-item')
+    .filter({ hasText: /cliente/i })
+    .click({ force: true });
+    const tipocad = await page.locator('input[aria-label="Tipo de cadastro"]').inputValue();      
+    console.log('✅ Tipo de cadastro selecionado:',tipocad);
+
+    await page.locator('.q-field')
+    .filter({ hasText: /departamento/i })
+    .first()
+    .click({ force: true });
+    const menuDepartamento = page.locator('.q-menu').last();
+    await menuDepartamento.waitFor();
+    await menuDepartamento
+    .locator('.q-item')
+    .filter({ hasText: /alto paraná|alto parana/i })
+    .click({ force: true });
+    const dep = await page.locator('input[aria-label="Departamento"]').inputValue();      
+    console.log('✅ Departamento:',dep);
+
+    await page.locator('.q-field')
+    .filter({ hasText: /distrito/i })
+    .first()
+    .click({ force: true });
+    const menuDistrito = page.locator('.q-menu').last();
+    await menuDistrito.waitFor();
+    await menuDistrito
+    .locator('.q-item')
+    .filter({ hasText: /ciudad/i })
+    .click({ force: true });
+    const distrito = await page.locator('input[aria-label="Distrito"]').inputValue();      
+    console.log('✅ Distrito:',distrito);
+
+    await page.locator('.q-field')
+    .filter({ hasText: /cidade/i })
+    .first()
+    .click({ force: true });
+    const menuCiudad = page.locator('.q-menu').last();
+    await menuCiudad.waitFor();
+    await menuCiudad
+    .locator('.q-item')
+    .filter({ hasText: /2A/i })
+    .click({ force: true });
+    const city = await page.locator('input[aria-label="Cidade/Bairro"]').inputValue();      
+    console.log('✅ Cidade:',city);
+
+    const direccion = `TEST DIRECCION ${Date.now()}`;
+    await page.getByLabel(/direção/i).fill(direccion);
+    console.log('✅ Endereço:', direccion);
+
+    await page.emulateMedia({ media: 'screen' });
+    await page.evaluate(() => {
+      document.body.style.zoom = '0.9';
+    });
+    console.log('🔍 Zoom ajustado para 90% via CSS');
+
+    const numero = Math.floor(Math.random() * 1000) + 1;
+    const campoNumero = page.locator('.q-field')
+    .filter({ hasText: /número/i })
+    .last();
+    await campoNumero.locator('input').fill(numero.toString());
+    console.log('✅ Número:', numero.toString().trim());
+
+    await page.waitForTimeout(1000);    
+
+    const telefone = Array.from({ length: 9 }, () =>
+      Math.floor(Math.random() * 10)
+    ).join('');    
+    const inputTelefone = page.locator('input[type="tel"]').first();
+    await inputTelefone.scrollIntoViewIfNeeded();
+    await inputTelefone.click({ force: true });
+    await inputTelefone.press('Control+A');
+    await inputTelefone.press('Backspace');
+    await inputTelefone.type(telefone, { delay: 30 });
+    console.log('✅ Telefone/Contato:', telefone);    
+    console.log('FIM DE DADOS ENVIADOS');           
+
+    await page.locator('.q-btn')
+    .filter({ hasText: /salvar|guardar/i })
+    .click({ force: true });
+    console.log('✅ Clicou em Salvar');            
+
+    const salvarPessoaResponse = await salvarPessoaPromise;
+    const dadosSalvos = await salvarPessoaResponse.json();
+    console.log('✅ DADOS RETORNADOS NA CRIAÇÃO');
+    console.log(JSON.stringify(dadosSalvos, null, 2));
+    
+    const idPessoa = dadosSalvos.pessoa.controle.toString().trim();
+    console.log('✅ CONTROLE:', idPessoa);    
+    const urlRegistroCriado = `https://testepyeduardo.global-hom.sgmw.com.br/api/py/pessoa/${idPessoa}`;    
+    const headersOriginais = salvarPessoaResponse.request().headers();
+    const headersGetRegistro: Record<string, string> = {
+      Accept: 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
+      authorization: headersOriginais['authorization'],
+      'x-xsrf-token': headersOriginais['x-xsrf-token'],
+      'x-tenant': headersOriginais['x-tenant'],
+      'x-empresa': headersOriginais['x-empresa'],
+    };
+    
+    const getCriadoResponse = await page.request.get(urlRegistroCriado, {
+      headers: headersGetRegistro,
+    });
+
+    console.log('✅ RESPOSTA DA API AO CONSULTAR O NOVO REGISTRO');
+    console.log(`Status: ${getCriadoResponse.status()}`);
+
+    try {
+      const dadosCriado = await getCriadoResponse.json();
+      console.log(JSON.stringify(dadosCriado, null, 2));
+    } catch (error) {
+      console.error('Erro ao converter resposta para JSON:', error);
+      const corpoBruto = await getCriadoResponse.text();
+      console.log('Corpo bruto da resposta:', corpoBruto);
+    }
+
+    expect([404, 200]).toContain(getCriadoResponse.status());    
+      
+   
+    await capturarRequisicoesApi(page); 
+    await page.waitForTimeout(4000);    
+
+function gerarRUC(): string {
+  const empresaAleatoria = empresasParaguai[Math.floor(Math.random() * empresasParaguai.length)];
+  return empresaAleatoria.ruc;
+}
+
+});
