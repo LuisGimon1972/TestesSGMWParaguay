55e951c90f593882e2ffcb2498f4214e281671ca
Luis Manuel <luismanuel@SGBR.intra>
2026-08-03 11:17:34 -0300

    Ajusta de exclusoes perfil

--- tests/Exclusao/exclusaoespecies.spec.ts
+++ tests/Exclusao/exclusaoespecies.spec.ts
@@ -62,13 +62,13 @@ test('Exclusão de datos espécies', async ({ page }) => {
     const getExcluidoResponse = await page.request.get(`/api/especie/${codigoLimpo}`);
 
     console.log('✅ RESPOSTA DA API AO CONSULTAR REGISTRO EXCLUÍDO');
-    console.log(`      ✅ Status: ${getExcluidoResponse.status()}`);
+    console.log(`     ✅ Status: ${getExcluidoResponse.status()}`);
 
     try {
     const dadosExcluido = await getExcluidoResponse.json();
     console.log(JSON.stringify(dadosExcluido, null, 2));
     } catch {
-    console.log('      ✅ Resposta sem corpo. (Status Code: 404)');
+    console.log('     ✅ Resposta sem corpo. (Status Code: 404)');
     }
     
     expect([404, 200]).toContain(getExcluidoResponse.status());
