e36208b7e44eedeb866950f05453b42aaa088f79
Luis Manuel <luismanuel@SGBR.intra>
2026-08-03 09:48:45 -0300

    Ajusta de exclusoes

--- tests/Exclusao/exclusaopessoas.spec.ts
+++ tests/Exclusao/exclusaopessoas.spec.ts
@@ -76,13 +76,13 @@ test('Exclusão de datos Pessoas', async ({ page }) => {
       const getExcluidoResponse = await page.request.get(`/api/py/pessoa/${codigoLimpo}`);
 
       console.log('✅ RESPOSTA DA API AO CONSULTAR REGISTRO EXCLUÍDO');
-      console.log(`   ✅ Status: ${getExcluidoResponse.status()}`);
+      console.log(`     ✅ Status: ${getExcluidoResponse.status()}`);
 
       try {
         const dadosExcluido = await getExcluidoResponse.json();
         console.log(JSON.stringify(dadosExcluido, null, 2));
       } catch {
-        console.log('   ✅ Resposta sem corpo. (Status Code: 404)');
+        console.log('     ✅ Resposta sem corpo. (Status Code: 404)');
       }
       
       expect([404, 200]).toContain(getExcluidoResponse.status());
