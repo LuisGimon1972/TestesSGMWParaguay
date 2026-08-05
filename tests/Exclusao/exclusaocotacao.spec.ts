2019aa938eac49ccc2b380003a3629317b124bd4
Luis Manuel <luismanuel@SGBR.intra>
2026-08-05 12:07:07 -0300

    Ajustar resposta API 2

--- tests/Exclusao/exclusaocotacao.spec.ts
+++ tests/Exclusao/exclusaocotacao.spec.ts
@@ -56,7 +56,7 @@ test('Exclusão de datos cotação de moedas', async ({ page }) => {
     await page.waitForTimeout(1000);
     await page.waitForSelector('button:has-text("EXCLUIR")');
     await page.click('button:has-text("EXCLUIR")');
-    console.log('✅ Clicou em Excluir no diálogo de confirmação');
+    console.log('✅ Clicou em Excluir no modal de confirmação');
 
     const deleteResponse = await page.waitForResponse((response) =>
     response.url().includes(`/api/moeda/cotacao/${codigoLimpo}`) &&
