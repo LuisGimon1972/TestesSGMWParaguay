03f93b2526cc04b2c35200251c57d0fddecbcbfa
Luis Manuel <luismanuel@SGBR.intra>
2026-08-06 16:15:00 -0300

    Ajustes informações adicionais

--- tests/Cadastros/produtos.spec.ts
+++ tests/Cadastros/produtos.spec.ts
@@ -131,12 +131,22 @@ test('Cadastro de produtos/serviços', async ({ page }) => {
       .click();
       const iva = await page.locator('input[aria-label="IVA"]').inputValue();      
       console.log('✅ IVA:',iva);   
-      
+
+
+      const textAreas = page.locator('textarea.q-field__native');
+
       await page.waitForTimeout(2000);
-      const obsproduto = `TEST OBSERVAÇÕES DE PRODUTOS PRODUTO REVISADO E APROVADO DE MUITA BOA QUALIDADE ${Date.now()}`;
-      await page.locator('textarea.q-field__native').fill(obsproduto);
+      const infproduto = `TEST INFORMAÇÕES ADICIONAIS DE PRODUTOS PRODUTO APROVADO DE MUITA BOA QUALIDADE ${Date.now()}`;
+      await textAreas.nth(0).fill(infproduto);
+      console.log('✅ Informações adicionais:', infproduto);
+      await expect(textAreas.nth(0)).toHaveValue(infproduto);      
+
+      await page.waitForTimeout(2000);
+      const obsproduto = `TEST OBSERVAÇÕES DE PRODUTOS PRODUTO REVISADO E APROVADO DE MUITA BOA QUALIDADE ${Date.now()}`;      
+      await textAreas.nth(1).fill(obsproduto);
       console.log('✅ Observações:', obsproduto);
-      await expect(page.locator('textarea.q-field__native')).toHaveValue(obsproduto);
+      await expect(textAreas.nth(1)).toHaveValue(obsproduto);
+
       console.log('➡️ FIM DE DADOS ENVIADOS');
 
       await page.locator('.q-btn')
